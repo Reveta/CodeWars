3257e81346ad1cd7fbc03ae7c66502869fcf1bb3
Reveta <gfdsre1999@gmail.com>
2021-12-08 20:47:33 +0200

    Commit1

--- 8kyu/ILoveYou,ALittleALotPassionately.cs
+++ 8kyu/ILoveYou,ALittleALotPassionately.cs
@@ -8,22 +8,36 @@ namespace CodeWars._8kyu {
 		 * https://www.codewars.com/kata/57f24e6a18e9fad8eb000296/train/csharp
 		 */
 		public static string HowMuchILoveYou(int nbPetals) {
-			var phrases = new List<string>() {
-				"I love you",
-				"a little",
-				"a lot",
-				"passionately",
-				"madly",
-				"not at all"
-			};
+			Console.WriteLine("Input " + nbPetals);
+			int count = _phrases.Count;
+			if (nbPetals > 0) {
+			} else {
+				_phrases.Reverse();
+			}
 
-			if (nbPetals <= 0) phrases.Reverse();
-			int petals = (Math.Abs(nbPetals) > phrases.Count
-				? nbPetals - phrases.Count * (nbPetals / phrases.Count)
-				: Math.Abs(nbPetals)) - 1;
+			nbPetals = nbPetals > 0 ? nbPetals : Math.Abs(nbPetals);
+			// Console.WriteLine(_phrases[0]);
+
+
+			int petals = (nbPetals > count ? nbPetals - count * (nbPetals / count) : nbPetals) - 1;
+			Thread.Sleep(1000);
+			if (petals >= 0) {
+				string howMuchILoveYou = _phrases[petals];
+				Console.WriteLine("Try Answer: " + howMuchILoveYou);
+				return petals >= 0 ? howMuchILoveYou : HowMuchILoveYou(petals);
+			}
 			
-			return petals >= 0 ? phrases[petals] : HowMuchILoveYou(petals);
+			return petals > 0 ? _phrases[petals] : HowMuchILoveYou(petals);
 		}
+
+		private static List<string> _phrases = new List<string>() {
+			"I love you",
+			"a little",
+			"a lot",
+			"passionately",
+			"madly",
+			"not at all"
+		};
 
 		public static void Main(string[] args) {
 			Console.WriteLine(HowMuchILoveYou(7));
