815dbed4f9edcc5735d70e55796678021033443a
Reveta <gfdsre1999@gmail.com>
2021-12-11 23:07:52 +0200

    +2tasks of 8kyu

---
+++ 8kyu/BeginnerSeries#1SchoolPaperwork.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeWars._8kyu {
+	public class BeginnerSeries_1SchoolPaperwork {
+		/**
+		 * https://www.codewars.com/kata/55f9b48403f6b87a7c0000bd
+		 */
+		public static int Paperwork(int n, int m) => n > 0 && m > 0 ? n * m : 0;
+	}
+}
---
+++ 8kyu/ThirdAngleOfATriangle.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeWars._8kyu {
+	public class ThirdAngleOfATriangle {
+		/**
+		 * https://www.codewars.com/kata/5a023c426975981341000014/train/csharp
+		 */
+		public static int OtherAngle(int a, int b)
+		{
+			return 180 - a - b;
+		}
+	}
+}
